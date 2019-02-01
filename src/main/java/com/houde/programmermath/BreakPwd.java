0cb950d5c6569cbe1e832d09ac702b8c4343431b
qiuhoude <3617150@qq.com>
2019-02-01 13:44:22 +0800

    程序员笔记

--- src/main/java/com/houde/programmermath/BreakPwd.java
+++ src/main/java/com/houde/programmermath/BreakPwd.java
@@ -4,14 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 暴力破解密码, 假设有一个4位字母密码，每位密码是a～e之间的小写字母
+ * 暴力破解密码, 假设有一个4位字母密码，每位密码是a～e之间的小写字母?
  *
  * @author qiukun
  * @create 2019-02-01 13:23
  */
 public class BreakPwd {
     private static final Logger logger = LoggerFactory.getLogger(CombinationCoin.class);
-    
+
     private static final char[] PWD_ELEMENT = {'a', 'b', 'c', 'd', 'e'};
 
     public static void main(String[] args) {
